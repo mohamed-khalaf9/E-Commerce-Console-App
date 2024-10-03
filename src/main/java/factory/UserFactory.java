516d3e6aec55b1b37346c13260f24a6d0a5773a0
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 00:49:40 +0300

    feat: add CreateUser() that takes user info and return UserModel object from type admin or customer based on the role

--- src/main/java/factory/UserFactory.java
+++ src/main/java/factory/UserFactory.java
@@ -1,4 +1,19 @@
 package factory;
+import model.*;
 
 public class UserFactory {
+
+    public static UserModel CreateUser(String name ,String email ,String password ,Role role)
+    {
+        switch(role)
+        {
+            case ADMIN:
+                return new AdminModel(name,email,password,role);
+                break;
+            case CUSTOMER:
+                return CustomerModel(name,email,password,role);
+        }
+        return null;
+
+    }
 }
