93616117ac6829c1cb49d3836df362de7f38d217
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-03 23:58:05 +0300

    feat: add accessSystem() in UserController to show start menu,handel signup , login or exit and return curUser to run() function in ECommerce

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -8,4 +8,57 @@ public class UserController {
     private UserView view;
     private UserService service;
     private UserModel currUser;
+
+    public UserController() {
+        view = new UserView();
+        service = new UserService();
+        currUser = new UserModel();
+    }
+
+    public UserModel accessSystem() {
+        try {
+            while (true) {
+                boolean isAccessSuccess = false;
+                int option = view.startMenu();
+
+                switch (option) {
+                    case 1:
+                        isAccessSuccess = signUP();
+                        if (isAccessSuccess) {
+                            currUser = service.getCurUser();
+                            view.showMessage("Sign Up Successful!");
+                            return currUser;
+                        }
+                        else {
+                            view.showMessage("User already exist , try again ...");
+                            continue;
+                        }
+                        break;
+                    case 2:
+                        isAccessSuccess = login();
+                        if (isAccessSuccess) {
+                            currUser = service.getCurUser();
+                            view.showMessage("Login Successful! Welcome, "+ currUser.getName());
+                            return currUser;
+                        }
+                        else {
+                            view.showMessage(" Email or Password is not correct , try again ...");
+                            continue;
+                        }
+                        break;
+                    default:
+                        throw new Exception();
+
+                }
+
+            }
+        } catch (Exception e) {
+            System.out.println("Exit successfully.");
+            System.exit(0);
+        }
+
+
+    }
 }
+
+
