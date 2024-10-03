2de206725af7ae950a99b2f3c2e4a82c9e4da5a7
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 02:59:02 +0300

    feat: add consturctor that takes UserModel that represent curCustomer logged in or signed up

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -4,4 +4,8 @@ import model.UserModel;
 
 public class CustomerService {
     private UserModel curCustomer;
+
+    public CustomerService(UserModel curCustomer) {
+        this.curCustomer = curCustomer;
+    }
 }
