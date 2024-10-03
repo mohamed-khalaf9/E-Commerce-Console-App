f80aebe167e7a74d00604aa5674807724953aa04
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 02:49:43 +0300

    feat: add run() function it act like entry point for admin features , and add constructor that takes AdminService

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -3,7 +3,15 @@ package controller;
 import service.AdminService;
 import view.AdminView;
 
-public class AdminController {
+public class AdminController extends BaseController {
     private AdminView view;
     private AdminService service;
+
+    public AdminController( AdminService service) {
+        this.service = service;
+    }
+
+    public void run(){
+
+    }
 }
