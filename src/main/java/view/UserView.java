037a95fff9de94cfd1e5238fbf4cb3267faeb9cd
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 00:09:24 +0300

    feat: add startMenu() in UserView to show start menu and allow user to enter choice and return this choice to accessSystem() in UserController

--- src/main/java/view/UserView.java
+++ src/main/java/view/UserView.java
@@ -1,4 +1,43 @@
 package view;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class UserView {
+
+    public int startMenu(){
+        int option = 0;
+        try{
+            while(true){
+                System.out.println("Welcome to the E-Commerce Console Application");
+                System.out.println("Please select an option:");
+                System.out.println("1. Sign UP");
+                System.out.println("2. Login");
+                System.out.println("3. Exit");
+
+                Scanner scanner = new Scanner(System.in);
+                option = scanner.nextInt();
+
+                if(option<1 || option>3){
+                    System.out.println("Invalid option");
+                    continue;
+                }
+                else {
+                    break;
+                }
+            }
+
+        }catch(InputMismatchException e){
+            System.out.println("Invalid option, try again....");
+            startMenu();
+
+        }
+        return option;
+
+    }
+
+    //public void showMessage(String message){
+
+    //}
+
 }
