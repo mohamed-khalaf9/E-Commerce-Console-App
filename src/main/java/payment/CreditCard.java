1966ba6d58d86bc3d26401a366668651e1f8cf57
roqia <elmaghrabyroqia2003@gmail.com>
2024-10-03 21:02:28 +0300

    feat:add Debit&Credit card data members (carsNumber ,expiryDate,cvv)and provide getters ,setters for them

--- src/main/java/payment/CreditCard.java
+++ src/main/java/payment/CreditCard.java
@@ -1,4 +1,31 @@
 package payment;
 
 public class CreditCard {
+    private String cardNumber;
+    private String expiryDate;
+    private int cvv;
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setCvv(int cvv) {
+        this.cvv = cvv;
+    }
+
+    public int getCvv() {
+        return cvv;
+    }
 }
