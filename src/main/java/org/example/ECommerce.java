e6a540de2f208ca1bb826081a929b47aba2a2f3d
roqia <elmaghrabyroqia2003@gmail.com>
2024-10-03 21:06:38 +0300

    feat: add ECommerce data members (controller and curUser)

--- src/main/java/org/example/ECommerce.java
+++ src/main/java/org/example/ECommerce.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import controller.UserController;
+import model.UserModel;
+import view.UserView;
+
 public class ECommerce {
+    private UserController controller;
+    private UserModel curUser;
+
 }
