980ad3b7a3a88da09b9b302c6b8a97d9281e6496
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 02:52:48 +0300

    feat: add run() function that acts as entry point for customer features and add constructor that takes service

--- src/main/java/controller/CustomerController.java
+++ src/main/java/controller/CustomerController.java
@@ -3,8 +3,23 @@ package controller;
 import service.CustomerService;
 import view.CustomerView;
 
-public class CustomerController {
+import java.util.Scanner;
+
+public class CustomerController extends BaseController {
     private CustomerView view;
     private CustomerService service;
+
+    public CustomerController( CustomerService service) {
+        this.service = service;
+    }
+
+    public void run(){
+        Scanner scanner = new Scanner(System.in);
+        while(true)
+        {
+
+        }
+
+    }
 
 }
