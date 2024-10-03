68f2fdacc39a0849113bb55a83fdc1a73dd1964f
roqia <elmaghrabyroqia2003@gmail.com>
2024-10-03 20:45:32 +0300

    feat: add UserService data members ( map of users with email as a key and UserModel object that has a getter to retrieve it easily)

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,14 @@
 package service;
 
+import model.UserModel;
+
+import java.util.HashMap;
+
 public class UserService {
+    private HashMap <String , UserModel> users;
+    private UserModel curUser;
+
+    public UserModel getCurUser() {
+        return curUser;
+    }
 }
