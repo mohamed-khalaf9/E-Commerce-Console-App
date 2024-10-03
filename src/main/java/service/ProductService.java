8a9055f82125b45dd2e518ecbdfe6bb03a7d45d4
Mohamed khalaf <149758845+mohamed-khalaf9@users.noreply.github.com>
2024-10-03 22:32:14 +0300

    Update ProductService.java
    just edit the syntax of map declaration

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProductService {
-    private HashMap < Integer , ProductModel> productId_productObj;
-    private HashMap <String , ArrayList<ProductModel> > category_products;
+    private Map < Integer , ProductModel> productId_productObj;
+    private Map <String , ArrayList<ProductModel> > category_products;
     private static ProductService instance;
 
     public static ProductService getInstance () {
