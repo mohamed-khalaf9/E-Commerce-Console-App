77423e344725b80b9f961d66b5f67c37710d1841
roqia <elmaghrabyroqia2003@gmail.com>
2024-10-03 18:39:12 +0300

    feat:add ProductService data members (map of products that stores each product with special key to facilitate retrieving these products) , (category map that stores products of specific category) and an instance of this class to prevent loosing data

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.ProductModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ProductService {
+    private HashMap < Integer , ProductModel> productId_productObj;
+    private HashMap <String , ArrayList<ProductModel> > category_products;
+    private static ProductService instance;
+
+    public static ProductService getInstance () {
+        return  instance;
+    }
+
+
 }
