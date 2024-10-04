c5ee9af7c3fce9218928dfefd6b5cd1db740e573
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 21:31:20 +0300

    feat: add showMenu() to AdminView as utality function for AdminController

--- src/main/java/view/AdminView.java
+++ src/main/java/view/AdminView.java
@@ -1,4 +1,37 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class AdminView {
+
+    public int showMenu(ArrayList<String> menu , String message)
+    {
+        Scanner input = new Scanner(System.in);
+        int option =0;
+        try{
+
+            while(true)
+            {
+                System.out.println(message);
+                for(int i=0;i<menu.size();i++)
+                {
+                    System.out.println((i+1)+menu.get(i));
+                }
+                option = input.nextInt();
+                if(option<1 || option>menu.size())
+                    continue;
+                else break;
+            }
+
+        }catch (InputMismatchException e)
+        {
+            System.out.println("Invalid input, try again .....");
+            showMenu(menu,message);
+        }
+
+       return option;
+    }
+
 }
