63743eeb978e816fba7a06f56fb6478f90d6f0e5
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-05 01:41:32 +0300

    feat: add updatedProductPrice() , updatedProductDescription() , updateProductStock() as part of update product info feature

--- src/main/java/service/AdminService.java
+++ src/main/java/service/AdminService.java
@@ -10,10 +10,24 @@ public class AdminService {
         this.curAdmin = curAdmin;
     }
 
-    public boolean addProduct(String name,double price,String description,int quantity,String category)
-    {
+    public boolean addProduct(String name, double price, String description, int quantity, String category) {
         ProductService productService = ProductService.getInstance();
-        return productService.addProduct(name,price,description,quantity,category);
+        return productService.addProduct(name, price, description, quantity, category);
     }
+
+    public boolean updateProductPrice(String category, ProductModel productToBeUpdated, double price){
+        ProductService productService = ProductService.getInstance();
+        return productService.updateProductPrice(category, productToBeUpdated,price);
+    }
+    public boolean updateProductDescription(String category, ProductModel productToBeUpdated, String description){
+        ProductService productService = ProductService.getInstance();
+        return productService.updateProductDescription(category, productToBeUpdated,description);
+    }
+    public boolean updateProductStock(String category, ProductModel productToBeUpdated, int stock){
+        ProductService productService = ProductService.getInstance();
+        return productService.updateProductStock(category, productToBeUpdated,stock);
+    }
+
+
 
 }
