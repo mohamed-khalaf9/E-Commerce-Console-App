ea146a81bffd2329f9896e0dae0b23dda674eac2
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 22:31:17 +0300

    fix: make price double

--- src/main/java/model/ProductModel.java
+++ src/main/java/model/ProductModel.java
@@ -3,7 +3,7 @@ package model;
 public class ProductModel {
     private static int id = 0;
     private String name;
-    private String price;
+    private double price;
     private String descreption;
     private String category;
     private int stock_quantity;
