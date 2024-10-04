5d6d6cce6f12406c9b499e5633b37e73d0710fdc
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 21:58:48 +0300

    refactor: add spaces between the number of the option and the option and modify option to handel logic error

--- src/main/java/view/AdminView.java
+++ src/main/java/view/AdminView.java
@@ -9,7 +9,7 @@ public class AdminView {
     public int showMenu(ArrayList<String> menu , String message)
     {
         Scanner input = new Scanner(System.in);
-        int option =0;
+        int option =-1;
         try{
 
             while(true)
@@ -17,7 +17,7 @@ public class AdminView {
                 System.out.println(message);
                 for(int i=0;i<menu.size();i++)
                 {
-                    System.out.println((i+1)+menu.get(i));
+                    System.out.println((i+1)+" "+menu.get(i));
                 }
                 option = input.nextInt();
                 if(option<1 || option>menu.size())
