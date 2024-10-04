b6e10ab84a80e1ba1557c3f483653affb48f3aa5
Mohamed khalaf <mohamed.khalaf8340@gmail.com>
2024-10-04 23:29:03 +0300

    feat: add addProduct() that implements the logic of adding product

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProductService {
-    private Map < Integer , ProductModel> productId_productObj;
-    private Map <String , ArrayList<ProductModel> > category_products;
+    private Map<Integer, ProductModel> productId_productObj;
+    private Map<String, ArrayList<ProductModel>> category_products;
     private static ProductService instance;
 
     private ProductService() {
@@ -16,13 +16,31 @@ public class ProductService {
         category_products = new HashMap<>();
     }
 
-    public static ProductService getInstance () {
-        if(instance == null) {
-           instance = new ProductService();
+    public static ProductService getInstance() {
+        if (instance == null) {
+            instance = new ProductService();
         }
 
-        return  instance;
+        return instance;
     }
 
+    public boolean addProduct(String name, double price, String description, int quantity, String category) {
 
+        ProductModel newProduct = new ProductModel(name, price, description, category, quantity);
+        productId_productObj.put(newProduct.getId(), newProduct);
+
+        if (category_products.containsKey(category)) {
+            category_products.get(category).add(newProduct);
+        } else {
+            category_products.put(category, new ArrayList<>());
+            category_products.get(category).add(newProduct);
+        }
+
+
+        return true;
+
+    }
 }
+
+
+
