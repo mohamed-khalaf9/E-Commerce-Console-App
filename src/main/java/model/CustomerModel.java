a5ac74bf60d4056fe510fe7c166e8fabe59eb561
roqia <elmaghrabyroqia2003@gmail.com>
2024-10-06 17:27:08 +0300

    fix:Edit CustomerModel constructor to inisialize instance of cartService in it

--- src/main/java/model/CustomerModel.java
+++ src/main/java/model/CustomerModel.java
@@ -1,27 +1,30 @@
 package model;
 
+import service.CartService;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerModel extends UserModel{
-    private CartModel customerCart;
+   // private CartModel customerCart;
+
     private Map<Integer ,OrderModel> customerOrders;
 
     public CustomerModel(String name,String email,String password ,Role role) {
         super(name,email,password,role);
         customerOrders = new HashMap<>();
-        customerCart = new CartModel();
-
+        //customerCart = new CartModel();
+           CartService.getInstance();
     }
 
 
-    public void setCustomerCart(CartModel _customerCart) {
+    /*public void setCustomerCart(CartModel _customerCart) {
         customerCart = _customerCart;
-    }
+    }*/
 
-    public CartModel getCustomerCart() {
+   /* public CartModel getCustomerCart() {
         return customerCart;
-    }
+    }*/
 
     public void setCustomerOrders(OrderModel order) {
          customerOrders.put(OrderModel.getId(),order);
