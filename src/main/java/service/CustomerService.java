218d6a16ca758341969ae40831edc34acf661689
roqia <elmaghrabyroqia2003@gmail.com>
2024-10-08 01:35:51 +0300

    feat:Add getCartItemsInfo function that return customer  cart items in formated way

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -15,8 +15,38 @@ public class CustomerService {
 
     }
 
-    public ProductModel[] BrowseProducts (String category){
+    public ProductModel[] BrowseProducts(String category) {
         return ProductService.getInstance().getProductsOfCategory(category);
 
     }
+
+    public boolean addToCart(ProductModel product, int quantity) {
+        try {
+            if (quantity > product.getStock_quantity()) {
+                throw new IllegalArgumentException("this quantity is greater than stock amount");
+            } else {
+                CartItemModel item = new CartItemModel(product, quantity);
+
+                if (curCustomer.getCustomerCart().isExist(item))
+                    throw new IllegalArgumentException("this item is already exists");
+
+                else {
+                    curCustomer.getCustomerCart().addItem(item);
+                    return true;
+                }
+            }
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+     public ArrayList<String> getCartItemsInfo (){
+        ArrayList<String>items =new ArrayList<>();
+         for (int i=0;i<curCustomer.getCustomerCart().getCartItems().size();i++){
+             items.add(curCustomer.getCustomerCart().getCartItems().get(i).ToString());
+         }
+         return items;
+     }
+}
    
